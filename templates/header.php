4c1179d5f2b7da011b321583b66f3a40be5a9826
ttbr101081 <ttbr101081@gmail.com>
2022-08-10 19:25:28 -0500

    cambio

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,6 @@
 <html lang="es">
-  <head>
+
+<head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -8,58 +9,66 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
     <style>
-      *{
-  margin: 0;
-  padding: 0;
-}
-header{
-  font-family: Helvetica;
-  width: 480px;
-  margin: 0 auto;
-}
-ul{
-  list-style: none;
-}
-#menu li>a{
-  background-color: grey;
-  color: white;
-  padding: 10px;
-  display: block;
-  text-decoration: none;
-  min-width: 100px;
-}
-#menu li>a:hover{
-  color: #000;
-  background-color: #eaeaea;
-}
-#menu>li{
-  float: left;
-  text-align:center
-}
-#menu>li>ul{
-  display: none;
-}
-#menu>li:hover>ul {
-  display:block;
-}
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+    header {
+        font-family: Helvetica;
+        width: 480px;
+        margin: 0 auto;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    #menu li>a {
+        background-color: grey;
+        color: white;
+        padding: 10px;
+        display: block;
+        text-decoration: none;
+        min-width: 100px;
+    }
+
+    #menu li>a:hover {
+        color: #000;
+        background-color: #eaeaea;
+    }
+
+    #menu>li {
+        float: left;
+        text-align: center
+    }
+
+    #menu>li>ul {
+        display: none;
+    }
+
+    #menu>li:hover>ul {
+        display: block;
+    }
     </style>
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="container">
-    <div class="row">
-    	<nav>
-        <ul id="menu">
-          <li><a href="">Inicio</a></li>
-          <li><a href="">Cursos</a>
-            <ul>
-              <li><a href="">Frontend</a></li>
-              <li><a href="">Backend</a></li>
-              <li><a href="">Mobile</a></li>
-            </ul>  
-          </li>
-          <li><a href="">Tutoriales</a></li>
-          <li><a href="">Contacto</a></li>
-        </ul>
-      </nav>
-      </div>
+        <div class="row">
+            <nav>
+                <ul id="menu">
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="">Cursos</a>
+                        <ul>
+                            <li><a href="">Frontend</a></li>
+                            <li><a href="">Backend</a></li>
+                            <li><a href="">Mobile</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">Tutoriales</a></li>
+                    <li><a href="">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
     </div>
